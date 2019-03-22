6f92c4166ad897d277d0d7aeacde3205d1f05be9
zenara <zenaronline@gmail.com>
2019-03-22 15:16:04 +0530

    Work Manager addition

---
+++ app/src/main/java/com/example/workmanager/UploadWorker.java
@@ -0,0 +1,20 @@
+package com.example.workmanager;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class UploadWorker extends Worker {
+
+    public UploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
+}
